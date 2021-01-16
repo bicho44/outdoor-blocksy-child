fa6a3a2381d76cc9a858c9c6e391f37c154ae88b
Federico Reinoso <bicho44@gmail.com>
2021-01-16 08:13:15 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,4 +4,6 @@ if (! defined('WP_DEBUG')) {
 }
 add_action( 'wp_enqueue_scripts', function () {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style', get_template_directory() . '/style.css' );
+
 });
