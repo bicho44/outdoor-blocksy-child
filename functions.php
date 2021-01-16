2bced0b869dbf9f51cc16570618668fb737e8bea
Federico Reinoso <bicho44@gmail.com>
2021-01-16 08:18:45 -0300

    Update functions.php
    Cambio de forma de carga de los estilos del Child Theme, via la info encontrada en https://developer.wordpress.org/themes/advanced-topics/child-themes/

--- functions.php
+++ functions.php
@@ -2,8 +2,20 @@
 if (! defined('WP_DEBUG')) {
 	die( 'Direct access forbidden.' );
 }
-add_action( 'wp_enqueue_scripts', function () {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'child-style', get_template_directory() . '/style.css' );
 
-});
+/* Loading Styles via Child Theme HandBook
+* @link: https://developer.wordpress.org/themes/advanced-topics/child-themes/
+*/
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    $parenthandle = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $theme = wp_get_theme();
+    wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
+        array(),  // if the parent theme code has a dependency, copy it to here
+        $theme->parent()->get('Version')
+    );
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
+        array( $parenthandle ),
+        $theme->get('Version') // this only works if you have Version in the style header
+    );
+}
